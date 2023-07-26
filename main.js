275d6530dbd479bc00753516d94c2aff5920334d
Avinash Kumar <avinashkumar@Avinashs-MacBook-Air.local>
2023-07-26 07:51:12 +0530

    First task

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings:["INTERNSHIP PROGRAM", "Web Development","App Development","C/C++ Development","Java Development","Cloud Computing"],
+    typeSpeed:90,
+    BackSpeed:60,
+    loop:true
+})
